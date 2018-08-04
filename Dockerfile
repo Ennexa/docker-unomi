e0a0f39730a7e7b5093689baecc5e67833860986
Joyce Babu <joyce@ennexa.com>
2018-08-04 15:33:03 +0530

    Updated to 1.2.0

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM tdraier/ubuntu-java
-MAINTAINER tdraier@jahia.com
+FROM java:8-jre
+MAINTAINER joyce@ennexa.com
 
 COPY install.sh /home/jahia/install.sh
-COPY startWithIp.sh /home/jahia/startWithIp.sh
 USER root
 WORKDIR /home/jahia
 RUN ./install.sh
+
+COPY startWithIp.sh /home/jahia/unomi/bin/startWithIp.sh
 WORKDIR /home/jahia/unomi/bin
 
 CMD ["sh", "./startWithIp.sh"]
-
+ENV KARAF_OPTS="-Dunomi.autoStart=true"
 EXPOSE 9443
 EXPOSE 8181
 EXPOSE 9200
 EXPOSE 9001
-
--- startWithIp.sh
+++ startWithIp.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 rm -f ../etc/org.oasis_open.contextserver.web.cfg
-IP=`/sbin/ifconfig eth0 | grep inet | awk '{print $2}' | sed 's/addr://' | grep .`
-if [ -n "$1" ]
+IP=$1
+if [ -z "$1" ]
 then
- IP=$1
+    IP=`/sbin/ifconfig eth0 | grep inet | awk '{print $2}' | sed 's/addr://' | grep .`
 fi
 echo contextserver.address=$IP >> ../etc/org.oasis_open.contextserver.web.cfg
 echo contextserver.port=8181 >> ../etc/org.oasis_open.contextserver.web.cfg
 echo contextserver.secureAddress=$IP >> ../etc/org.oasis_open.contextserver.web.cfg
 echo contextserver.securePort=9443 >> ../etc/org.oasis_open.contextserver.web.cfg
-./karaf
+
+# export KARAF_DEBUG=true
+./karaf ${2:-start}
